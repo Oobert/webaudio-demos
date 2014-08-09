4d682cfb5252e3dea51aba03512f117100be88dd
Tony Gemoll <noobert@gmail.com>
2014-08-09 12:21:27 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+/*var Hapi = require('hapi');
+var server = new Hapi.Server(3000, {files: {etagsCacheMaxSize: 0}});
+
+server.route({
+    path: '/{param}',
+    method: 'GET',
+    handler: { directory: { path: './demos'}}
+});
+
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+*/
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+port = process.argv[2] || 3000;
+
+http.createServer(function(request, response) {
+
+    var uri = url.parse(request.url).pathname
+        , filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
+                response.end();
+                return;
+            };
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
